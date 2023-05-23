6e22b921a5218af157df6b2b5f23f8b33336875a
Marco Antônio <marco1002@live.com>
2023-05-23 18:51:23 -0300

    pegando checklists e labels

--- ParseJsonTrelloToHtmlTable/Controllers/FileController.cs
+++ ParseJsonTrelloToHtmlTable/Controllers/FileController.cs
@@ -35,7 +35,9 @@ namespace ParseJsonTrelloToHtmlTable.Controllers
 
             var model = JsonSerializer.Deserialize<TrelloDTO>(jsonAsString);
 
-            var html = @"<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css' integrity='sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65' crossorigin='anonymous'><table class=table table-bordered><tbody>";
+            var html = @"<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.css'>
+                <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css' integrity='sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65' crossorigin='anonymous'>
+                <table class=table table-bordered><tbody>";
 
             int index = 1;
             foreach (var (listDTO, index1) in model.Lists.Select((TrelloDTO.ListDTO listDTO, int index) => (listDTO, index)))
@@ -44,15 +46,49 @@ namespace ParseJsonTrelloToHtmlTable.Controllers
 
                 html += $@"<tr>
                     <td align='center'><b>{(index1 + 1)}</b></td>
-                    <td align='center' colspan='2'><strong>{listDTO.Nome}</strong></td>
+                    <td align='center' colspan='3'><strong>{listDTO.Nome}</strong></td>
                  </tr>";
 
                 foreach (var (card, index2) in cards.Select((TrelloDTO.CardDTO listDTO, int index) => (listDTO, index)))
                 {
+                    var checklists = model.Checklists.Where(x => card.ChecklistIds.Contains(x.Id)).ToList();
+
+                    var checkListHtml = "";
+
+                    if (checklists.Any())
+                    {
+                        checkListHtml = "<ol>";
+
+                        foreach (var checklist in checklists)
+                        {
+                            checkListHtml += $"<li>{checklist.Nome}<ol>";
+                            foreach (var item in checklist.Itens)
+                            {
+                                checkListHtml += $"<li><i class='fa {(item.Status == "complete" ? "fa-check text-success" : "fa-times text-danger")}'></i> - {item.Nome}</li>";
+                            }
+                            checkListHtml += "</ol></li>";
+
+                            checkListHtml += "<ol>";
+                        }
+                    }
+
+                    var labels = model.Labels.Where(x => card.LabelIds.Contains(x.Id)).ToList();
+
+                    var labelsHtml = "";
+
+                    if (labels.Any())
+                    {
+                        foreach (var label in labels)
+                        {
+                            labelsHtml += $"<span class='badge' style='background: {label.Cor}; color: {(label.Cor == "yellow" ? "black" : "white")}; display:inherit;'>{label.Nome}</span>";
+                        }
+                    }
+
                     html += $@"<tr>
                         <td align='center'>{(index2 + 1)}</td>
                         <td align='center'>{index++}</td>
-                        <td>{card.Titulo}</td>
+                        <td>{card.Titulo}<br>{checkListHtml}</td>
+                        <td>{labelsHtml}</td>
                      </tr>";
                 }
             }
@@ -71,6 +107,45 @@ namespace ParseJsonTrelloToHtmlTable.Controllers
         [JsonPropertyName("lists")]
         public List<ListDTO> Lists { get; set; }
 
+        [JsonPropertyName("labels")]
+        public List<LabelDTO> Labels { get; set; }
+
+        [JsonPropertyName("checklists")]
+        public List<ChecklistDTO> Checklists { get; set; }
+
+        public class ChecklistDTO
+        {
+            [JsonPropertyName("name")]
+            public string Nome { get; set; }
+
+            [JsonPropertyName("id")]
+            public string Id { get; set; }
+
+            [JsonPropertyName("checkItems")]
+            public List<ChecklistItemDTO> Itens { get; set; }
+
+            public class ChecklistItemDTO
+            {
+                [JsonPropertyName("name")]
+                public string Nome { get; set; }
+
+                [JsonPropertyName("state")]
+                public string Status { get; set; }
+            }
+        }
+
+        public class LabelDTO
+        {
+            [JsonPropertyName("name")]
+            public string Nome { get; set; }
+
+            [JsonPropertyName("id")]
+            public string Id { get; set; }
+
+            [JsonPropertyName("color")]
+            public string Cor { get; set; }
+        }
+
         public class ListDTO
         {
             [JsonPropertyName("name")]
@@ -87,6 +162,12 @@ namespace ParseJsonTrelloToHtmlTable.Controllers
 
             [JsonPropertyName("idList")]
             public string ListId { get; set; }
+
+            [JsonPropertyName("idChecklists")]
+            public List<string> ChecklistIds { get; set; }
+
+            [JsonPropertyName("idLabels")]
+            public List<string> LabelIds { get; set; }
         }
     }
 }
