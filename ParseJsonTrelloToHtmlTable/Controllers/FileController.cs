69123a1918305a80877c0b3d8f716d784d71541e
Marco Antônio <marco1002@live.com>
2023-05-23 17:43:44 -0300

    Add project files.

---
+++ ParseJsonTrelloToHtmlTable/Controllers/FileController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace ParseJsonTrelloToHtmlTable.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileController : Controller
+    {
+        public async Task<string> ReadAsStringAsync(IFormFile file)
+        {
+            var result = new StringBuilder();
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                while (reader.Peek() >= 0)
+                    result.AppendLine(await reader.ReadLineAsync());
+            }
+            return result.ToString();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormFile arquivo)
+        {
+            var jsonAsString = await ReadAsStringAsync(arquivo);
+
+
+            return View();
+        }
+    }
+}
